f4f9aa797426a0c2232cd61ea11376bfd8e32e68
hubeen <mhubeen@gmail.com>
2019-06-17 13:04:21 +0900

    first commit

---
+++ qwer/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 800
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(201, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(79, 21)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(201, 39)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(78, 21)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 44)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "작동"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 12)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(147, 102)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 44)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "중지"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 75)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(267, 21)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = "2018-01-30-10-18-00"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 152)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ qwer/Form1.vb
@@ -0,0 +1,141 @@
+﻿Imports System
+Imports System.Runtime.InteropServices
+
+
+
+Public Class Form1
+    <DllImport("user32")>
+    Public Shared Function GetCursorPos(ByRef pt As POINT) As Int32
+    End Function
+    <DllImport("user32")>
+    Public Shared Function SetCursorPos(x As Int32, y As Int32) As Int32
+    End Function
+    Public Structure POINT
+        Public x As Int32
+        Public y As Int32
+    End Structure
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Dim pt As POINT
+    Dim tDate As Date
+    Dim strings As String
+    Dim strings0 As String
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        tDate = Now()
+        GetCursorPos(pt)
+        strings = Format(tDate, "yyyy-MM-dd-HH-mm-ss")
+
+        Label2.Text = strings
+        Label1.Text = "x: " & pt.x & vbCrLf & "y: " & pt.y
+        If String.Equals(strings, TextBox3.Text) Then
+            Dim vOut1 As Int32 = Convert.ToInt16(TextBox1.Text)
+            Dim vOut2 As Int32 = Convert.ToInt16(TextBox2.Text)
+            MouseMove(vOut1, vOut2)
+            MouseLeftClick()
+        End If
+
+    End Sub
+    Public Shared Sub MouseMove(ByVal x As Integer, ByVal y As Integer)
+        Dim fx = x * (65535.0# / (GetSystemMetrics(SystemMetric.SM_CXSCREEN) - 1))
+        Dim fy = y * (65535.0# / (GetSystemMetrics(SystemMetric.SM_CYSCREEN) - 1))
+        Dim it As New INPUT
+        it.dwType = INPUT_MOUSE
+        it.mkhi.mi.dwFlags = MOUSEEVENTF_MOVE Or MOUSEEVENTF_ABSOLUTE
+        it.mkhi.mi.dx = fx
+        it.mkhi.mi.dy = fy
+        SendInput(1, it, Marshal.SizeOf(it))
+    End Sub
+
+    Public Shared Sub MouseLeftClick()
+        Dim it As New INPUT
+        it.dwType = INPUT_MOUSE
+        it.mkhi.mi.dwFlags = MOUSEEVENTF_LEFTDOWN
+        SendInput(1, it, Marshal.SizeOf(it))
+        Application.DoEvents()
+        it = New INPUT
+        it.dwType = INPUT_MOUSE
+        it.mkhi.mi.dwFlags = MOUSEEVENTF_LEFTUP
+        SendInput(1, it, Marshal.SizeOf(it))
+    End Sub
+
+    Private Structure INPUT
+        Dim dwType As Integer
+        Dim mkhi As MOUSEKEYBDHARDWAREINPUT
+    End Structure
+
+    <StructLayout(LayoutKind.Explicit)>
+    Private Structure MOUSEKEYBDHARDWAREINPUT
+        <FieldOffset(0)> Public mi As MOUSEINPUT
+        <FieldOffset(0)> Public ki As KEYBDINPUT
+        <FieldOffset(0)> Public hi As HARDWAREINPUT
+    End Structure
+
+    Private Structure MOUSEINPUT
+        Public dx As Integer
+        Public dy As Integer
+        Public mouseData As Integer
+        Public dwFlags As Integer
+        Public time As Integer
+        Public dwExtraInfo As Integer
+    End Structure
+
+    Private Structure KEYBDINPUT
+        Public wVk As Short
+        Public wScan As Short
+        Public dwFlags As Integer
+        Public time As Integer
+        Public dwExtraInfo As Integer
+    End Structure
+
+    Private Structure HARDWAREINPUT
+        Public uMsg As Integer
+        Public wParamL As Short
+        Public wParamH As Short
+    End Structure
+
+    Const INPUT_MOUSE As UInt32 = 0
+    Const INPUT_KEYBOARD As Integer = 1
+    Const INPUT_HARDWARE As Integer = 2
+    Const XBUTTON1 As UInt32 = &H1
+    Const XBUTTON2 As UInt32 = &H2
+    Const MOUSEEVENTF_MOVE As UInt32 = &H1
+    Const MOUSEEVENTF_LEFTDOWN As UInt32 = &H2
+    Const MOUSEEVENTF_LEFTUP As UInt32 = &H4
+    Const MOUSEEVENTF_RIGHTDOWN As UInt32 = &H8
+    Const MOUSEEVENTF_RIGHTUP As UInt32 = &H10
+    Const MOUSEEVENTF_MIDDLEDOWN As UInt32 = &H20
+    Const MOUSEEVENTF_MIDDLEUP As UInt32 = &H40
+    Const MOUSEEVENTF_XDOWN As UInt32 = &H80
+    Const MOUSEEVENTF_XUP As UInt32 = &H100
+    Const MOUSEEVENTF_WHEEL As UInt32 = &H800
+    Const MOUSEEVENTF_VIRTUALDESK As UInt32 = &H4000
+    Const MOUSEEVENTF_ABSOLUTE As UInt32 = &H8000
+
+    <DllImport("user32.dll")> Private Shared Function GetSystemMetrics(ByVal smIndex As Integer) As Integer
+    End Function
+    <DllImport("user32.dll")> Private Shared Function SendInput(ByVal nInputs As Integer, ByRef pInputs As INPUT, ByVal cbSize As Integer) As Integer
+    End Function
+
+    Public Enum SystemMetric As Integer
+        ''' <summary>
+        '''  Width of the screen of the primary display monitor in pixels. This is the same values obtained by calling GetDeviceCaps as follows: GetDeviceCaps( hdcPrimaryMonitor HORZRES).
+        ''' </summary>
+        SM_CXSCREEN = 0
+        ''' <summary>
+        ''' Height of the screen of the primary display monitor in pixels. This is the same values obtained by calling GetDeviceCaps as follows: GetDeviceCaps( hdcPrimaryMonitor VERTRES).
+        ''' </summary>
+        SM_CYSCREEN = 1
+    End Enum
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+    End Sub
+End Class
